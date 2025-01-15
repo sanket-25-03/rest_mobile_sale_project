20993f1fb773da64c345cdb4286e10c2d72f828c
sanket96s <sanketm9797@gmail.com>
2025-01-15 17:00:55 +0530

    final today

--- mobile_project/mobile_sale/urls.py
+++ mobile_project/mobile_sale/urls.py
@@ -1,17 +1,20 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 from .views import ProductListView, ProductCreateView, OrderCreateView, index, add_product_view, order_list, create_order_view
 
 urlpatterns = [
     path('', index, name='index'),  # Home page
-    path('products/', ProductListView.as_view(), name='product-list'),  # Product list view
+    path('products/', ProductListView.as_view(), name='product-list'),
     path('products/create/', ProductCreateView.as_view(), name='product-create'),  # Product creation view
     path('place-order/', OrderCreateView.as_view(), name='place-order'),  # Order creation view
     path('add-product/', add_product_view, name='add-product'),  # Add product view
     path('orders/create/', create_order_view, name='order-create'),  
     path('orders/', order_list, name='order-list'),  # Order list view
+    path('create-order/', views.create_order, name='create_order'),
+
 ]
 
 if settings.DEBUG:
