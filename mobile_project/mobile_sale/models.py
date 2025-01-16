e875329f5f50fc8b89917fe266d5be4ae771a413
sanket96s <sanketm9797@gmail.com>
2025-01-16 13:02:19 +0530

    final

--- mobile_project/mobile_sale/models.py
+++ mobile_project/mobile_sale/models.py
@@ -5,16 +5,11 @@ class User(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=50)
     
-    def __str__(self):
-        return self.username
-
 class Reviews(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='reviews_by_username')
     email = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='reviews_by_email')
     reviews = models.CharField(max_length=200)
     
-    def __str__(self):
-        return f"Review by {self.username}"
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -24,9 +19,6 @@ class Product(models.Model):
     reviews = models.ManyToManyField(Reviews, blank=True)
     quantity = models.IntegerField()
     prod_image = models.ImageField(upload_to='products/', null=True)
-    
-    def __str__(self):
-        return self.name
     
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
