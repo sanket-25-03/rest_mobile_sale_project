fdf704345ba3931bb31c55e727b2cfe5084f5549
sanket96s <sanketm9797@gmail.com>
2025-01-21 12:14:38 +0530

    added all methods for all models

--- mobile_project/mobile_sale/urls.py
+++ mobile_project/mobile_sale/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
     path('mobile/', views.ProductView.as_view(), name='ProductView'), 
+    path('review/', views.ReviewView.as_view(), name='ReviewView'), 
+    path('Inventory/', views.InventoryView.as_view(), name='InventoryView'), 
+    path('order/', views.OrderView.as_view(), name='OrderView'), 
+    path('order-item/', views.OrderItemView.as_view(), name='OrderItemView'), 
 ]
 
 if settings.DEBUG:
