d66298b6f679a1f8ab36aa0821339d9a598eec85
sanket96s <sanketm9797@gmail.com>
2025-01-22 15:43:21 +0530

    all data filled in the database

--- mobile_project/mobile_sale/urls.py
+++ mobile_project/mobile_sale/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('mobile/', views.ProductView.as_view(), name='ProductView'), 
     path('review/', views.ReviewView.as_view(), name='ReviewView'), 
     path('Inventory/', views.InventoryView.as_view(), name='InventoryView'), 
-    path('user/', views.UserView.as_view(), name='UserView'), 
     path('order/', views.OrderView.as_view(), name='OrderView'), 
     path('register/', views.RegisterAPI.as_view(), name='RegisterAPI'),
     path('login/', views.LoginAPI.as_view(), name='LoginAPI'),
