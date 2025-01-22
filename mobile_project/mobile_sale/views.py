e05abad371a330faccacc04faba2ed12296dedf1
sanket96s <sanketm9797@gmail.com>
2025-01-22 15:12:17 +0530

    s

--- mobile_project/mobile_sale/views.py
+++ mobile_project/mobile_sale/views.py
@@ -77,7 +77,7 @@ class ReviewView(APIView):
     def post(self, request):
         serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
