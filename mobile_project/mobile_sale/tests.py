f1dfaac993baf568ff03152154c1a5b1d3e4fc1f
sanket96s <sanketm9797@gmail.com>
2025-01-24 11:14:40 +0530

    s

--- mobile_project/mobile_sale/tests.py
+++ mobile_project/mobile_sale/tests.py
@@ -2,20 +2,19 @@ from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from django.contrib.auth.models import User
-from .models import Product, Inventory
-
+from .models import Product
 
 class ProductCreateAPITest(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.url = reverse('product-create')
         self.valid_payload = {
-        "prod_image": None,
-        "product_name": "string",
-        "brand": "string",
-        "price": "-.4",
-        "short_description": "string",
-        "category": "string"
+            "prod_image": None,
+            "product_name": "string",
+            "brand": "string",
+            "price": "-.4",
+            "short_description": "string",
+            "category": "string"
         }
         self.invalid_payload = {
             'name': '',
@@ -31,17 +30,11 @@ class ProductCreateAPITest(APITestCase):
         response = self.client.post(self.url, self.invalid_payload, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-
 class ReviewCreateAPITest(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.product = Product.objects.create(product_name="string",
-            brand="string",
-            price="-24022760.60",
-            short_description="string",
-            category="string"
-        )        
+        self.product = Product.objects.create(product_name="string", brand="string", price="-24022760.60", short_description="string", category="string")        
         self.client.force_authenticate(user=self.user)
         self.url = reverse('review-create')
         self.valid_payload = {
@@ -66,29 +59,23 @@ class ReviewCreateAPITest(APITestCase):
         response = self.client.post(self.url, self.invalid_payload, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-
 class InventoryCreateAPITest(APITestCase):
     def setUp(self):
         self.client = APIClient()
-        self.product = Product.objects.create(product_name="string",
-            brand="string",
-            price="-24022760.60",
-            short_description="string",
-            category="string"
-        )
+        self.product = Product.objects.create(product_name="string", brand="string", price="-24022760.60", short_description="string", category="string")        
         self.url = reverse('inventory-create')
         self.valid_payload = {
-        "imei_number": "string",
-        "detailed_info": "string",
-        "stock_quantity": 2147483647,
-        "os": "string",
-        "ram": "string",
-        "storage": "string",
-        "battery_capacity": "string",
-        "screen_size": "string",
-        "camera_details": "string",
-        "processor": "string",
-        "product": self.product.id
+            "imei_number": "string",
+            "detailed_info": "string",
+            "stock_quantity": 2147483647,
+            "os": "string",
+            "ram": "string",
+            "storage": "string",
+            "battery_capacity": "string",
+            "screen_size": "string",
+            "camera_details": "string",
+            "processor": "string",
+            "product": self.product.id
 }
         self.invalid_payload = {
             'product': self.product.id,
@@ -103,26 +90,20 @@ class InventoryCreateAPITest(APITestCase):
         response = self.client.post(self.url, self.invalid_payload, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-
 class OrderCreateAPITest(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.product = Product.objects.create(product_name="string",
-            brand="string",
-            price="-24022760.60",
-            short_description="string",
-            category="string"
-        )        
+        self.product = Product.objects.create(product_name="string", brand="string", price="-24022760.60", short_description="string", category="string")         
         self.client.force_authenticate(user=self.user)
         self.url = reverse('order-create')
         self.valid_payload = {
-        "shipping_address": "string",
-        "total_price": "-29832899",
-        "status": "pending",
-        "payment_method": "string",
-        "payment_status": "string",
-        "user": self.user.id,
+            "shipping_address": "string",
+            "total_price": "-29832899",
+            "status": "pending",
+            "payment_method": "string",
+            "payment_status": "string",
+            "user": self.user.id,
         }
         self.invalid_payload = {
             'product': self.product.id,
@@ -136,7 +117,6 @@ class OrderCreateAPITest(APITestCase):
     def test_create_invalid_order(self):
         response = self.client.post(self.url, self.invalid_payload, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
 
 class RegisterAPITest(APITestCase):
     def setUp(self):
@@ -160,8 +140,7 @@ class RegisterAPITest(APITestCase):
     def test_register_invalid_user(self):
         response = self.client.post(self.url, self.invalid_payload, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-
+        
 class LoginAPITest(APITestCase):
     def setUp(self):
         self.client = APIClient()
