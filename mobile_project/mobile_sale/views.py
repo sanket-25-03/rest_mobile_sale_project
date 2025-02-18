9d1f0b3299d6037f671c68dc7c2bf6c1fa0346ed
KEDAR BIRAJDAR <120714345+kedarb7079@users.noreply.github.com>
2025-02-18 10:57:44 +0530

    Revert "Merge branch 'main' into kedar"
    This reverts commit 2aac0c6060374252b4af40fd1a7b37d5d01ad12d, reversing
    changes made to 8b3a72b7dcbb1a8ad4a52d6eb39fb07ddcb76e8f.

--- mobile_project/mobile_sale/views.py
+++ mobile_project/mobile_sale/views.py
@@ -4,34 +4,33 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate
 from .models import Product, Reviews, Inventory, Order
-from .serializers import (
-    ProductSerializer, ReviewSerializer, InventorySerializer, OrderSerializer, 
-    RegisterSerializer, LoginSerializer, UserSerializer
-)
+from .serializers import ProductSerializer, ReviewSerializer, InventorySerializer, OrderSerializer, RegisterSerializer, LoginSerializer, UserSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import GenericAPIView
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.settings import api_settings
+from .serializers import LoginSerializer, UserSerializer
+from rest_framework.pagination import PageNumberPagination
+from django.contrib.auth.models import User
 from .pagination import CustomPagination
-
 
 class ProductAPIView(GenericAPIView):
     serializer_class = ProductSerializer
-
     def get(self, request, pk=None):
         if pk:
             product = get_object_or_404(Product, pk=pk)
-            serializer = self.serializer_class(product)
+            serializer = ProductSerializer(product)
         else:
             products = Product.objects.all()
-            serializer = self.serializer_class(products, many=True)
+            serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
     def put(self, request, pk=None):
         if not pk:
             return Response({"message": "ID is required for update"}, status=status.HTTP_400_BAD_REQUEST)
         product = get_object_or_404(Product, pk=pk)
-        serializer = self.serializer_class(product, data=request.data, partial=True)
+        serializer = ProductSerializer(product, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -46,39 +45,29 @@ class ProductAPIView(GenericAPIView):
 
 class ProductCreateAPIView(GenericAPIView):
     serializer_class = ProductSerializer
-    pagination_class = CustomPagination
-
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, request):
-        products = Product.objects.all()
-        paginator = self.pagination_class()
-        paginated_products = paginator.paginate_queryset(products, request)
-        serializer = self.serializer_class(paginated_products, many=True)
-        return paginator.get_paginated_response(serializer.data)
-
 class ReviewAPIView(GenericAPIView):
-    serializer_class = ReviewSerializer
-
+    serializer_class  = ReviewSerializer
     def get(self, request, pk=None):
         if pk:
             review = get_object_or_404(Reviews, pk=pk)
-            serializer = self.serializer_class(review)
+            serializer = ReviewSerializer(review)
         else:
             reviews = Reviews.objects.all()
-            serializer = self.serializer_class(reviews, many=True)
+            serializer = ReviewSerializer(reviews, many=True)
         return Response(serializer.data)
 
     def put(self, request, pk=None):
         if not pk:
             return Response({"message": "ID is required for update"}, status=status.HTTP_400_BAD_REQUEST)
         review = get_object_or_404(Reviews, pk=pk)
-        serializer = self.serializer_class(review, data=request.data, partial=True)
+        serializer = ReviewSerializer(review, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -93,40 +82,29 @@ class ReviewAPIView(GenericAPIView):
 
 class ReviewCreateAPIView(GenericAPIView):
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated]
-    pagination_class = CustomPagination
-
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, request):
-        reviews = Reviews.objects.all()
-        paginator = self.pagination_class()
-        paginated_reviews = paginator.paginate_queryset(reviews, request)
-        serializer = self.serializer_class(paginated_reviews, many=True)
-        return paginator.get_paginated_response(serializer.data)
-
 class InventoryAPIView(GenericAPIView):
     serializer_class = InventorySerializer
-
     def get(self, request, pk=None):
         if pk:
             inventory = get_object_or_404(Inventory, pk=pk)
-            serializer = self.serializer_class(inventory)
+            serializer = InventorySerializer(inventory)
         else:
             inventories = Inventory.objects.all()
-            serializer = self.serializer_class(inventories, many=True)
+            serializer = InventorySerializer(inventories, many=True)
         return Response(serializer.data)
 
     def put(self, request, pk=None):
         if not pk:
             return Response({"message": "ID is required for update"}, status=status.HTTP_400_BAD_REQUEST)
         inventory = get_object_or_404(Inventory, pk=pk)
-        serializer = self.serializer_class(inventory, data=request.data, partial=True)
+        serializer = InventorySerializer(inventory, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -140,22 +118,13 @@ class InventoryAPIView(GenericAPIView):
         return Response({"success": f"Inventory with ID {pk} has been deleted."}, status=status.HTTP_200_OK)
 
 class InventoryCreateAPIView(GenericAPIView):
-    serializer_class = InventorySerializer
-    pagination_class = CustomPagination
-
+    serializer_class   = InventorySerializer
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        serializer = InventorySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def get(self, request):
-        inventories = Inventory.objects.all()
-        paginator = self.pagination_class()
-        paginated_inventories = paginator.paginate_queryset(inventories, request)
-        serializer = self.serializer_class(paginated_inventories, many=True)
-        return paginator.get_paginated_response(serializer.data)
 
 class OrderAPIView(GenericAPIView):
     serializer_class = OrderSerializer
@@ -164,17 +133,17 @@ class OrderAPIView(GenericAPIView):
     def get(self, request, pk=None):
         if pk:
             order = get_object_or_404(Order, pk=pk, user=request.user)
-            serializer = self.serializer_class(order)
+            serializer = OrderSerializer(order)
         else:
             orders = Order.objects.filter(user=request.user)
-            serializer = self.serializer_class(orders, many=True)
+            serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data)
 
     def put(self, request, pk=None):
         if not pk:
             return Response({"message": "ID is required for update"}, status=status.HTTP_400_BAD_REQUEST)
         order = get_object_or_404(Order, pk=pk, user=request.user)
-        serializer = self.serializer_class(order, data=request.data, partial=True)
+        serializer = OrderSerializer(order, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -190,46 +159,90 @@ class OrderAPIView(GenericAPIView):
 class OrderCreateAPIView(GenericAPIView):
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
-    pagination_class = CustomPagination
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        serializer = OrderSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, request):
-        orders = Order.objects.filter(user=request.user)
-        paginator = self.pagination_class()
-        paginated_orders = paginator.paginate_queryset(orders, request)
-        serializer = self.serializer_class(paginated_orders, many=True)
-        return paginator.get_paginated_response(serializer.data)
-
 class RegisterAPI(GenericAPIView):
     serializer_class = RegisterSerializer
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response({'user': UserSerializer(user).data}, status=status.HTTP_201_CREATED)
-        return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        data = request.data
+        serializer = RegisterSerializer(data=data)
+        
+        if not serializer.is_valid():
+            return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'user': UserSerializer(user).data}, status=status.HTTP_201_CREATED)
 
-class LoginAPI(GenericAPIView):
+#token
+class LoginAPI(APIView):
     serializer_class = LoginSerializer
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        data = request.data
+        serializer = self.serializer_class(data=data)
+
         if not serializer.is_valid():
             return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
         user = authenticate(username=serializer.validated_data['username'], password=serializer.validated_data['password'])
+
         if not user:
             return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
         refresh = RefreshToken.for_user(user)
+        access_token_expiry = refresh.access_token['exp'] * 1000  
+        refresh_token_expiry = refresh['exp'] * 1000
+
         return Response({
             'access_token': str(refresh.access_token),
             'refresh_token': str(refresh),
-            'access_token_expiry': api_settings.ACCESS_TOKEN_LIFETIME.total_seconds(),
-            'refresh_token_expiry': api_settings.REFRESH_TOKEN_LIFETIME.total_seconds(),
+            'access_token_expiry': access_token_expiry,
+            'refresh_token_expiry': refresh_token_expiry
         }, status=status.HTTP_200_OK)
+
+
+#pagination
+class UnifiedPaginatedAPI(GenericAPIView):
+    serializer_class = None
+    pagination_class = CustomPagination
+
+    def get(self, request):
+        model = request.query_params.get("model", "").lower()
+
+        if model == "product":
+            queryset = Product.objects.all()
+            serializer_class = ProductSerializer
+        elif model == "review":
+            queryset = Reviews.objects.all()
+            serializer_class = ReviewSerializer
+        elif model == "inventory":
+            queryset = Inventory.objects.all()
+            serializer_class = InventorySerializer
+        elif model == "order":
+            queryset = Order.objects.filter(user=request.user)
+            serializer_class = OrderSerializer
+        elif model == "user":
+            queryset = User.objects.all()
+            serializer_class = UserSerializer
+        else:
+            return Response(
+                {"error": "Invalid model specified. Choose from ['product', 'review', 'inventory', 'order', 'user']."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(queryset, request)
+        if page is not None:
+            serializer = serializer_class(page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+
+        serializer = serializer_class(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
